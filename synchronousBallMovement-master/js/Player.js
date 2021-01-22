32c37265fd9215ba15cf4f3784f56243c77c16b3
srushteeM <59560681+srushteeM@users.noreply.github.com>
2021-01-22 16:33:30 +0530

    Update Player.js

--- synchronousBallMovement-master/js/Player.js
+++ synchronousBallMovement-master/js/Player.js
@@ -3,22 +3,5 @@ class Player{
 
     }
 
-    getCount(){
-        db.ref("playerCount").on("value",function(data){
-           playerCount=data.val();
-        })
-    }
-
-     updateCount(count){
-        db.ref("/").update({
-            playerCount:count
-        })
-    }
-
-    updateName(name){
-        var playerIndex="player"+playerCount;
-        db.ref(playerIndex).set({
-            name:name
-        })
-    }
+   
 }
